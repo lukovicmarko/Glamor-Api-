84412b252ea1e730f063b9090183f6168b23f1ba
marko lukovic <lukovic86@gmail.com>
2021-01-03 17:41:46 +0100

    product model refactor

--- backend/data/products.js
+++ backend/data/products.js
@@ -2,18 +2,26 @@ const products = [
   {
     name: 'Classic Pullover Men\'s Hoodie',
     image: [
-      'assets/images/19.jpg',
-      'assets/images/20.jpg',
-      'assets/images/21.jpg',
-      'assets/images/16.jpg',
-      'assets/images/17.jpg',
-      'assets/images/18.jpg',
-      'assets/images/22.jpg',
-      'assets/images/23.jpg',
-      'assets/images/24.jpg',
-      'assets/images/25.jpg',
-      'assets/images/26.jpg',
-      'assets/images/27.jpg',
+      [
+        'assets/images/19.jpg',
+        'assets/images/20.jpg',
+        'assets/images/21.jpg',
+      ],
+      [
+        'assets/images/16.jpg',
+        'assets/images/17.jpg',
+        'assets/images/18.jpg'
+      ],
+      [
+        'assets/images/22.jpg',
+        'assets/images/23.jpg',
+        'assets/images/24.jpg'
+      ],
+      [
+        'assets/images/25.jpg',
+        'assets/images/26.jpg',
+        'assets/images/27.jpg'
+      ],
     ],
     description:
       'Men\'s classic pullover from basic collection. Put on over the head. Made of pleasant and soft material. Perfectly adapts to the figure. Round neckline. Bottom and sleeves of hoodie finished with welt. Comfortable cut makes the hoodie perfect for everyday wear.',
@@ -27,15 +35,21 @@ const products = [
   {
     name: 'Women\'s Hoodie Trendyol',
     image: [
-      'assets/images/7.jpg',
-      'assets/images/8.jpg',
-      'assets/images/9.jpg',
-      'assets/images/10.jpg',
-      'assets/images/11.jpg',
-      'assets/images/12.jpg',
-      'assets/images/13.jpg',
-      'assets/images/14.jpg',
-      'assets/images/15.jpg',
+      [
+        'assets/images/7.jpg',
+        'assets/images/8.jpg',
+        'assets/images/9.jpg'
+      ],
+      [
+        'assets/images/10.jpg',
+        'assets/images/11.jpg',
+        'assets/images/12.jpg'
+      ],
+      [
+        'assets/images/13.jpg',
+        'assets/images/14.jpg',
+        'assets/images/15.jpg'
+      ],
     ],
     description:
       'Very lightweight multifunctional sweatshirt, ideal for active recreation. Flexible and lightweight material ensures a comfortable fit for a variety of sports activities.',
@@ -49,12 +63,16 @@ const products = [
   {
     name: 'Campri Junior Snow Boots',
     image: [
-      'assets/images/1.jpg',
-      'assets/images/2.jpg',
-      'assets/images/3.jpg',
-      'assets/images/4.jpg',
-      'assets/images/5.jpg',
-      'assets/images/6.jpg',
+      [
+        'assets/images/1.jpg',
+        'assets/images/2.jpg',
+        'assets/images/3.jpg'
+      ],
+      [
+        'assets/images/4.jpg',
+        'assets/images/5.jpg',
+        'assets/images/6.jpg'
+      ],
     ],
     description:
       'Campri Junior Snow Boots Perfect for winter the Campri Junior Snow Boots feature a snowproof upper with a moulded rubber sole for traction even in deep snow.',
